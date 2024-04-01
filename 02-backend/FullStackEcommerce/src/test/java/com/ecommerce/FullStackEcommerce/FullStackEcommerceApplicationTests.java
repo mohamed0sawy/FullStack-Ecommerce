c9e43a2e2b9aa5792cf47c356313a8a8b90764c2
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-04-01 14:36:47 +0200

    first commit

---
+++ 02-backend/FullStackEcommerce/src/test/java/com/ecommerce/FullStackEcommerce/FullStackEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.FullStackEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
