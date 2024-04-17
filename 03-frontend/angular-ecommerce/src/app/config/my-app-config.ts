8e3b5a2d0b05c68315d76d1c9678c8ee0b56f450
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-04-17 21:47:06 +0200

    add login, logout feature using OKTA

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oagia8bp7jdOdxFQ5d7',
+    issuer: 'http://dev-05978289.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
